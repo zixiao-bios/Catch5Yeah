fcfd829ff7b958254dd3e3a22786dd6042e4e7e4
zixiao_bios <zixiao_bios@outlook.com>
2022-01-22 19:52:57 +0800

    mark demos

--- Software/src/main.cpp
+++ Software/src/main.cpp
@@ -9,19 +9,20 @@
 RGB_Strip *rgb1, *rgb3;
 
 [[noreturn]] void mainTask(void *pv) {
-    rgb1 = new RGB_Strip(1);
-    rgb3 = new RGB_Strip(3);
-    rgb1->inverse = true;
-    rgb1->setEffect(RGB_THEATER_RAINBOW);
-    rgb3->setEffect(RGB_THEATER_RAINBOW);
-    while (true) {
-        delay(3000);
-        rgb1->setEffect(RGB_OFF);
-        rgb3->setEffect(RGB_OFF);
-        delay(3000);
-        rgb1->setEffect(RGB_THEATER_RAINBOW);
-        rgb3->setEffect(RGB_THEATER_RAINBOW);
-    }
+    /// RGB demo
+//    rgb1 = new RGB_Strip(1);
+//    rgb3 = new RGB_Strip(3);
+//    rgb1->inverse = true;
+//    rgb1->setEffect(RGB_THEATER_RAINBOW);
+//    rgb3->setEffect(RGB_THEATER_RAINBOW);
+//    while (true) {
+//        delay(3000);
+//        rgb1->setEffect(RGB_OFF);
+//        rgb3->setEffect(RGB_OFF);
+//        delay(3000);
+//        rgb1->setEffect(RGB_THEATER_RAINBOW);
+//        rgb3->setEffect(RGB_THEATER_RAINBOW);
+//    }
 }
 
 __attribute__((unused)) void setup() {
@@ -30,6 +31,7 @@ __attribute__((unused)) void setup() {
     pinMode(2, OUTPUT);
     digitalWrite(2, LOW);
 
+    /// Display demo
 //    displayInit();
 //    displayTaskRun();
 
