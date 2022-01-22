ba41706749dbec731235addb9a65dcb7a10a3b38
zixiao_bios <zixiao_bios@outlook.com>
2022-01-22 21:02:58 +0800

    add WiFi scan demo

--- Software/src/main.cpp
+++ Software/src/main.cpp
@@ -5,10 +5,43 @@
 #include "Display.h"
 #include "RGB_Strip.h"
 
+#include <WiFi.h>
+
 
 RGB_Strip *rgb1, *rgb3;
 
 [[noreturn]] void mainTask(void *pv) {
+    WiFiClass::mode(WIFI_STA);
+    WiFi.disconnect();
+    delay(100);
+    Serial.println("setup done");
+
+    while (true) {
+        Serial.println("scan start");
+        int n = WiFi.scanNetworks();
+        Serial.println("snan done");
+
+        if (n == 0) {
+            Serial.println("no WiFi found");
+        } else {
+            Serial.print(n);
+            Serial.println(" networks found");
+
+            for (int i = 0; i < n; ++i) {
+                Serial.print(i + 1);
+                Serial.print(": ");
+                Serial.print(WiFi.SSID(i));
+                Serial.print(" (");
+                Serial.print(WiFi.RSSI(i));
+                Serial.print(")");
+                Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
+                delay(10);
+            }
+        }
+        Serial.println("=======================================\n\n");
+        delay(1000);
+    }
+
     /// RGB demo
 //    rgb1 = new RGB_Strip(1);
 //    rgb3 = new RGB_Strip(3);
@@ -28,8 +61,11 @@ RGB_Strip *rgb1, *rgb3;
 __attribute__((unused)) void setup() {
     Serial.begin(115200);
 
-    pinMode(2, OUTPUT);
-    digitalWrite(2, LOW);
+    pinMode(K1, INPUT);
+    pinMode(K2, INPUT);
+    pinMode(K3, INPUT);
+    pinMode(K4, INPUT);
+    pinMode(BTN, INPUT);
 
     /// Display demo
 //    displayInit();
