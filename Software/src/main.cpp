613802440ce1514c82a421db3a3b0fd136bedde0
zixiao_bios <zixiao_bios@outlook.com>
2022-01-28 20:04:13 +0800

    add WiFi demo

--- Software/src/main.cpp
+++ Software/src/main.cpp
@@ -4,42 +4,83 @@
 #include "Audio.h"
 #include "Display.h"
 #include "RGB_Strip.h"
+#include "../MyConfig.h"
 
 #include <WiFi.h>
 
-
+const String url = "https://www.runoob.com/http/http-tutorial.html";
 RGB_Strip *rgb1, *rgb3;
+
+void printWifiList() {
+    Serial.println("scan start");
+    int n = WiFi.scanNetworks();
+    Serial.println("snan done");
+
+    if (n == 0) {
+        Serial.println("no WiFi found");
+    } else {
+        Serial.print(n);
+        Serial.println(" networks found");
+
+        for (int i = 0; i < n; ++i) {
+            Serial.print(i + 1);
+            Serial.print(": ");
+            Serial.print(WiFi.SSID(i));
+            Serial.print(" (");
+            Serial.print(WiFi.RSSI(i));
+            Serial.print(")");
+            Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? " " : "*");
+            delay(10);
+        }
+    }
+    Serial.println("=======================================\n\n");
+}
+
+void wifiConnect() {
+    Serial.print("WiFi connecting");
+    WiFi.begin(SSID, PASSWORD);
+    while (WiFiClass::status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
 
 [[noreturn]] void mainTask(void *pv) {
     WiFiClass::mode(WIFI_STA);
-    WiFi.disconnect();
-    delay(100);
-    Serial.println("setup done");
+    wifiConnect();
 
     while (true) {
-        Serial.println("scan start");
-        int n = WiFi.scanNetworks();
-        Serial.println("snan done");
-
-        if (n == 0) {
-            Serial.println("no WiFi found");
-        } else {
-            Serial.print(n);
-            Serial.println(" networks found");
-
-            for (int i = 0; i < n; ++i) {
-                Serial.print(i + 1);
-                Serial.print(": ");
-                Serial.print(WiFi.SSID(i));
-                Serial.print(" (");
-                Serial.print(WiFi.RSSI(i));
-                Serial.print(")");
-                Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
-                delay(10);
-            }
+        if (WiFiClass::status() != WL_CONNECTED) {
+            wifiConnect();
         }
-        Serial.println("=======================================\n\n");
-        delay(1000);
+
+        HTTPClient http;
+
+        Serial.println("[HTTP] begin...");
+        http.begin(url);
+
+        Serial.println("[HTTP] GET...");
+        int httpCode = http.GET();
+
+        if (httpCode > 0) {
+            // HTTP header has been send and Server response header has been handled
+            Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+            // file found at server
+            if (httpCode == HTTP_CODE_OK) {
+                String payload = http.getString();
+                Serial.println(payload);
+            }
+        } else {
+            Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+        }
+
+        http.end();
+        delay(5000);
     }
 
     /// RGB demo
@@ -71,7 +112,7 @@ __attribute__((unused)) void setup() {
 //    displayInit();
 //    displayTaskRun();
 
-    xTaskCreatePinnedToCore(mainTask, "mainTask", 4096, nullptr, 1, nullptr, 0);
+    xTaskCreatePinnedToCore(mainTask, "mainTask", 10000, nullptr, 1, nullptr, 0);
 }
 
 __attribute__((unused)) void loop() {
